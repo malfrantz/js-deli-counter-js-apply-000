1f665a6e681a00cbdde5f6c2a9b9611a5e414c21
Mallory Frantz <mallory.rosenberger@gmail.com>
2018-07-27 03:02:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-var katzDeli = [];
+function takeANumber(katzDeli, name){
+  katzDeli.push(name) ;
 
-function line(array) {
-  if(array.length < 1) {
-    return "The line is currently empty." ;
+  if(katzDeli.length > 0) {
+    return "Welcome, " + name + ". " + "You are number " + katzDeli.length + " in line." ;
   } else {
-    var queue = [] ;
-
-  array.forEach(function(name, index) {
-    queue.push((index + 1).toString() + ". " + name);
-   });
-   var response = "The line is currently: " + queue.join(", ")
-   return response;
+    return "The line is currently empty." ;
   }
  }
