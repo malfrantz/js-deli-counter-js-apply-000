71580f04b6ae0b57cb69c02e325ad0ab0bd31e31
Mallory Frantz <mallory.rosenberger@gmail.com>
2018-07-27 03:04:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ function takeANumber(katzDeli, name){
     return "The line is currently empty." ;
   }
  }
+ 
+ function nowServing(array){
+  if(array.length === 0) {
+    var response = "There is nobody waiting to be served!";
+    console.log(response);
+    return response;
+
+  } else {
+    var person = array.shift() ;
+    var myResponse = "Currently serving " + person + "." ;
+    return myResponse ;
+  }
+ }
